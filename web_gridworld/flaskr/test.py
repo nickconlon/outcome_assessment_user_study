c06ef568d166ddea6657c08571fd238d41038630
Nicholas <nicholas.conlon@colorado.edu>
2021-07-29 18:00:53 +0000

    fixing merge conflict

--- web_gridworld/flaskr/test.py
+++ web_gridworld/flaskr/test.py
@@ -32,19 +32,24 @@ def dump():
 
 def del_entry(id):
     # Create a SQL connection to our SQLite database
-    con = sqlite3.connect("../../instance/flaskr.sqlite")
+    con = sqlite3.connect(DB_FILE)
 
     cur = con.cursor()
 
     # The result of a "cursor.execute" can be iterated over by row
     cur.execute('DELETE FROM user where id=?', (id,))
-    cur.execute('DELETE FROM results where id=?', (id,))
-
+    cur.execute('DELETE FROM results where user_id=?', (id,))
+    con.commit()
     # Be sure to close the connection
     con.close()
 
 if __name__ == "__main__":
     dump()
+    #del_entry(1)
+    #del_entry(2)
+    #del_entry(5)
+    #del_entry(6)
+    #dump()
     #cleanup()
     #dump()
     pass
